eac3c856a212fab708a44a164e4b6f768a51334c
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-10-28 12:24:26 +0100

    Adding a document.ready function to wrap it

--- js/timeline-nav.js
+++ js/timeline-nav.js
@@ -4,33 +4,35 @@
   var nextPoint = $('#next').attr('href');
   var prevPoint = $('#prev').attr('href');
 
-  document.getElementById('next').onclick = function() {
-    if (next != 29) {
-      next++;
-    }
-    if (prev != 28) {
-      prev++;
-    }
-    document.getElementById('next').setAttribute("href", next);
-    document.getElementById('prev').setAttribute("href", prev);
+  $(document).ready(function() {
+    document.getElementById('next').onclick = function() {
+      if (next != 29) {
+        next++;
+      }
+      if (prev != 28) {
+        prev++;
+      }
+      document.getElementById('next').setAttribute("href", next);
+      document.getElementById('prev').setAttribute("href", prev);
 
-    $('html, body').animate({
-      scrollTop: $(nextPoint).offset().top - 200
-    }, 1000);
-  };
+      $('html, body').animate({
+        scrollTop: $(nextPoint).offset().top - 200
+      }, 1000);
+    };
 
-  document.getElementById('prev').onclick = function() {
-    if (next != 1) {
-      next--;
-    }
-    if (prev != 0) {
-      prev--;
-    }
-    document.getElementById('next').setAttribute("href", next);
-    document.getElementById('prev').setAttribute("href", prev);
-    $('html, body').animate({
-      scrollTop: $(prevPoint).offset().top - 200
-    }, 1000);
-  };
+    document.getElementById('prev').onclick = function() {
+      if (next != 1) {
+        next--;
+      }
+      if (prev != 0) {
+        prev--;
+      }
+      document.getElementById('next').setAttribute("href", next);
+      document.getElementById('prev').setAttribute("href", prev);
+      $('html, body').animate({
+        scrollTop: $(prevPoint).offset().top - 200
+      }, 1000);
+    };
+  });
 
 })(jQuery);

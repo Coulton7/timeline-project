b10ff3e109983cc2c2357fb8cc33e2254f400c5d
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-10-28 15:04:18 +0100

    New js for timeline nav and moved image to correct id

--- js/timeline-nav.js
+++ js/timeline-nav.js
@@ -1,31 +1,74 @@
 (function($) {
-  var next = 1;
-  var prev = 0;
-  var nextPoint = $('#next').attr('href');
-  var prevPoint = $('#prev').attr('href');
+  var stickyTop = 0;
+  var scrollTarget = false;
 
-  $(document).ready(function() {
-    document.getElementById('next').onclick = function() {
-      if (next != 29) {
-        next++;
-      }
-      if (prev != 28) {
-        prev++;
-      }
-      document.getElementById('next').setAttribute("href", "#" + next);
-      document.getElementById('prev').setAttribute("href", "#" + prev);
-    };
+  var timeline_nav = $('.timeline-nav');
+  var items = $('li', timeline_nav);
+  var milestones = $('.timeline li');
+  var offsetTop = parseInt(timeline_nav.css('top'));
 
-    document.getElementById('prev').onclick = function() {
-      if (next != 1) {
-        next--;
-      }
-      if (prev != 0) {
-        prev--;
-      }
-      document.getElementById('next').setAttribute("href", "#" + next);
-      document.getElementById('prev').setAttribute("href", "#" + prev);
-    };
+  var TIMELINE_VALUES = {
+    start: 190,
+    step: 30
+  };
+
+  $(window).resize(function() {
+    timeline_nav.removeClass('fixed');
+
+    stickyTop = timeline_nav.offset().top - offsetTop;
+
+    $(window).trigger('scroll');
+  }).trigger('resize');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > stickyTop) {
+      timeline_nav.addClass('fixed');
+    } else {
+      timeline_nav.removeClass('fixed');
+    }
+  }).trigger('scroll');
+
+  items.find('span').click(function() {
+    var li = $(this).parent();
+    var index = li.index();
+    var milestone = milestones.eq(index);
+
+    if (!li.hasClass('active') && milestone.length) {
+      scrollTarget = index;
+
+      var scrollTargetTop = milestone.offset().top - 80;
+
+      $('html, body').animate({
+        scrollTop: scrollTargetTop
+      }, {
+        duration: 400,
+        complete: function complete() {
+          scrollTarget = false;
+        }
+      });
+    }
   });
 
+  $(window).scroll(function() {
+    var viewLine = $(window).scrollTop() + $(window).height() / 3;
+    var  active = -1;
+
+    if (scrollTarget === false) {
+      milestones.each(function() {
+        if ($(this).offset().top - viewLine > 0) {
+          return false;
+        }
+
+        active++;
+      });
+    } else {
+      active = scrollTarget;
+    }
+
+    timeline_nav.css('top', -1 * active * TIMELINE_VALUES.step + TIMELINE_VALUES.start + 'px');
+
+    items.filter('.active').removeClass('active');
+
+    items.eq(active != -1 ? active : 0).addClass('active');
+  }).trigger('scroll');
 })(jQuery);
